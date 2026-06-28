d57de7cf956f384dc1d81883f5f3ba97866ac0fb
17Artist <652805973@qq.com>
2026-06-29 05:50:46 +0800

    feat: 基岩粒子语句支持坐标

--- src/main/kotlin/priv/seventeen/artist/arcartx/hook/mythicmobs/mechanic/BedrockParticle.kt
+++ src/main/kotlin/priv/seventeen/artist/arcartx/hook/mythicmobs/mechanic/BedrockParticle.kt
@@ -9,14 +9,23 @@
 
 package priv.seventeen.artist.arcartx.hook.mythicmobs.mechanic
 
+import io.lumine.mythic.api.adapters.AbstractLocation
 import io.lumine.mythic.api.config.MythicLineConfig
+import io.lumine.mythic.api.skills.ITargetedLocationSkill
 import io.lumine.mythic.api.skills.SkillMetadata
+import io.lumine.mythic.api.skills.SkillResult
+import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.core.skills.mechanics.CustomMechanic
 import org.bukkit.entity.Entity
+import priv.seventeen.artist.arcartx.core.effect.ArcartXEffectManager.spawnBedrockParticle
+import priv.seventeen.artist.arcartx.core.effect.data.EffectPosition
+import priv.seventeen.artist.arcartx.nms.AsteroidScheduler.ensureMainThread
 import priv.seventeen.artist.arcartx.util.EntityUtils.doWithSeenBy
 import priv.seventeen.artist.arcartx.util.PlayerUtils.arcartXHandler
+import priv.seventeen.artist.blink.bukkitPlugin
 
-class BedrockParticle(holder: CustomMechanic?, mlc: MythicLineConfig) : AbstractEntityMechanic(holder) {
+class BedrockParticle(holder: CustomMechanic?, mlc: MythicLineConfig) : AbstractEntityMechanic(holder),
+    ITargetedLocationSkill {
 
     private val id = mlc.getPlaceholderString(arrayOf("id", "pid"), "")
 
@@ -24,5 +33,13 @@ class BedrockParticle(holder: CustomMechanic?, mlc: MythicLineConfig) : Abstract
         entity.doWithSeenBy() { player ->
             player.arcartXHandler?.spawnBedrockParticle(id.get(skillMetadata), entity.location.x, entity.location.y, entity.location.z, entity.location.yaw, 0F)
         }
+    }
+
+    override fun castAtLocation(skillMetadata: SkillMetadata, abstractLocation: AbstractLocation): SkillResult {
+        val location = BukkitAdapter.adapt(abstractLocation)
+        bukkitPlugin.ensureMainThread {
+            location.world?.spawnBedrockParticle(location, id.get(skillMetadata), EffectPosition.location(location))
+        }
+        return SkillResult.SUCCESS
     }
 }
